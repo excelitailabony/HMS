e2e72536efba24a06d98b73f2a53a0aea99dad2d
excelitai-kamrul <kamrul@excelitai.com>
2021-12-04 09:54:15 +0600

    patient

--- app/Http/Controllers/Admin/ReceptionistController.php
+++ app/Http/Controllers/Admin/ReceptionistController.php
@@ -5,149 +5,154 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Receptionist;
-use Image;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
+use Image; 
+use Illuminate\Validation\Rules\Password;
+
 
 class ReceptionistController extends Controller
 {
-    //
-    
-    // Receptionist View
+    // method for all patient data 
     public function ReceptionistView(){
+        $patients = Receptionist::latest()->get();
 
-        $receptionists= Receptionist::latest()->get();
-    
-        return View('super_admin.receptionist.view_receptionist', compact('receptionists'));
-    
-        }// end method
+        // dd($patients);
+        return view('super_admin.receptionist.view_receptionist',compact('patients'));
+    }
 
- // store Receptionist
-  public function ReceptionistStore(Request $request){   
-      
-    // validation 
-        $request->validate([
-            'name' => 'required', 
-            'email' => 'required', 
-            'password' => 'required', 
-            'address' => 'required', 
-            'phone' => 'required',  
-            'sex' => 'required', 
-            'dob' => 'required',     
-            'age' => 'required', 
-            'bloodgrp' => 'required',        
-            'image' => 'required',       
-          ]);
-          // img upload and save
-          $image = $request->file('image');
-          $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-          Image::make($image)->resize(300,300)->save('upload/receptionist/'.$name_gen);
-          $save_url = 'upload/receptionist/'.$name_gen;
+    // method for storing patient data
+    public function ReceptionistStore(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:100',
+            'email' => 'required|unique:patients|email',
+            'password' => [
+                        'required',
+                        Password::min(8)
+                        ->letters()
+                        ->numbers()
+                    ],
+            'address' => 'required',
+            'phone' => 'required|numeric|digits_between: 1,11',
+            'dob' => 'required',
+            'blood_group' => 'required',
+            'gender' => 'required',
+            'age' => 'required|numeric',
+        ]);
+        if($validator->fails())
+        {
+            return response()->json([
+                'status'=>400,
+                'errors'=>$validator->messages()
+            ]);
+        }
+        else{
+            $patient=new Receptionist;
+            $patient->name=$request->input('name');
+            $patient->email=$request->input('email');
+            $patient->password=$request->input('password');
+            $patient->address=$request->input('address');
+            $patient->phone=$request->input('phone');
+            $patient->sex=$request->input('gender');
+            $patient->dob=$request->input('dob');
+            $patient->age=$request->input('age');
+            $patient->bloodgrp=$request->input('blood_group');
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $extension = hexdec(uniqid()).'.'.$file->getClientOriginalExtension();
+                Image::make($file)->resize(300,300)->save('uploads/patient/'.$extension);
+                $save_url = 'uploads/patient/'.$extension;
+                $patient->image= $save_url;
+            }
+            $patient->save();
+            return response()->json([
+               'status'=>200,
+               'message'=>'Patient Updated Successfully.'
+           ]);
+        }
+    }
 
-       // Accountant Insert    
-       Receptionist::insert([
+    // method for editing patient data
+    public function ReceptionistEdit($id){
+        $patient = Receptionist::find($id);
+        return response()->json([
+            'status' =>200,
+            'patient' => $patient,
+        ]);
+    }
 
-           'name' => $request->name,   
-           'email' => $request->email,
-           'password' => $request->password, 
-           'address' => $request->address, 
-           'phone' => $request->phone, 
-           'sex' => $request->sex, 
-           'dob' => $request->dob, 
-           'age' => $request->age, 
-           'bloodgrp' => $request->bloodgrp,            
-           'image' => $save_url,  
+    // method for updating data
+    public function ReceptionistUpdate(Request $request){
+        // dd($request->all());
+        $old_img  = $request->old_image;
+        unlink($old_img);
+        $image = $request->file('image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(300,300)->save('uploads/receptionist/'.$name_gen);
+        $save_url = 'uploads/receptionist/'.$name_gen;
 
-          ]); 
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:100',
+            'email' => 'required|email',
+            // 'password' => [
+            //             'required',
+            //             Password::min(8)
+            //             ->letters()
+            //             ->numbers()
+            //         ],
+            'address' => 'required',
+            'phone' => 'required|numeric|digits_between: 1,11',
+            'dob' => 'required',
+            'blood_group' => 'required',
+            'gender' => 'required',
+            'age' => 'required|numeric',
 
-          $notification = array(
-            'message' =>  'Receptionist Added Successfuly',
-            'alert-type' => 'success'
+        ]);
+        if($validator->fails())
+        {
+            return response()->json([
+                'status'=>400,
+                'errors'=>$validator->messages()
+            ]);
+        }
+        else
+        {
+            $patient_id=$request->input('patient_id');
+            $patient = Receptionist::find($patient_id);
+
+            $patient->name = $request->input('name');
+            $patient->email=$request->input('email');
+            $patient->address=$request->input('address');
+            $patient->phone=$request->input('phone');
+            $patient->sex=$request->input('gender');
+            $patient->dob=$request->input('dob');
+            $patient->age=$request->input('age');
+            $patient->blood_group=$request->input('blood_group');
+            $patient->image=$save_url;
+            $patient->update();
+            return response()->json([
+                'status'=>200,
+                'message'=>'Patient Updated Successfully.'
+            ]);
+
+        }      
+    }
+
+    // method for deleting patient data
+     public function ReceptionistDelete($id){
+
+        $patient = Receptionist::findOrFail($id);
+        if($patient->image){
+             $img = $patient->image;
+            unlink($img);
+        }
+
+        Receptionist::findOrFail($id)->delete();
+        $notification = array(
+            'message' =>  'Patient Delete Sucessyfully',
+            'alert-type' => 'info'
         ); 
+        return redirect()->back()->with($notification);
+    } 
 
-
-        return Redirect()->back()->with($notification);        
-
-  } // end method 
-
-  // method for editing accountant data
-  public function ReceptionistEdit($id){
-    $receptionist = Receptionist::find($id);
-    return response()->json([
-        'status' =>200,
-        'receptionist' => $receptionist,
-    ]);
-}
-
-
-
- // method for updating data
- public function ReceptionistUpdate(Request $request){
-  $old_img  = $request->old_image;
-
-  if ($request->file('image')) {
-
-    unlink($old_img);
-    $image = $request->file('image');
-    $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-    Image::make($image)->resize(300,300)->save('upload/receptionist/'.$name_gen);
-    $save_url = 'upload/receptionist/'.$name_gen;
-  
-   
-// image
-  $receptionist_id=$request->input('receptionist_id');
-  $receptionist =Receptionist::find($receptionist_id);
-  $receptionist->name=$request->name;
-  $receptionist->email=$request->email;
-  $receptionist->password=$request->password;
-  $receptionist->address=$request->address;
-  $receptionist->phone=$request->phone;
-  $receptionist->sex=$request->sex;
-  $receptionist->dob=$request->dob;
-  $receptionist->age=$request->age;
-  $receptionist->bloodgrp=$request->bloodgrp;
-  $receptionist->image=$save_url;
-  $receptionist->update();
-
-   $notification=array(
-      'message'=>'Receptionist Updated Success',
-      'alert-type'=>'success'
-  );
-
-  return Redirect()->back()->with($notification);
-}
-else{
-  $receptionist_id=$request->input('receptionist_id');
-  $receptionist =Receptionist::find($receptionist_id);
-  $receptionist->name=$request->name;
-  $receptionist->email=$request->email;
-  $receptionist->password=$request->password;
-  $receptionist->address=$request->address;
-  $receptionist->phone=$request->phone;
-  $receptionist->sex=$request->sex;
-  $receptionist->dob=$request->dob;
-  $receptionist->age=$request->age;
-  $receptionist->bloodgrp=$request->bloodgrp;
-  
-  $receptionist->update();
-
-   $notification=array(
-      'message'=>'Receptionist Updated Success',
-      'alert-type'=>'success'
-  );
-
-  return Redirect()->back()->with($notification);
-}
-}
-// delete
-public function ReceptionistDelete($id){
-
-  $receptionist = Receptionist::findOrFail($id);
-
-            $img = $receptionist->image;
-            unlink($img);  
-            Receptionist::findOrFail($id)->delete(); 
-            return redirect()->back();
-
- 
-}
-  
 }
